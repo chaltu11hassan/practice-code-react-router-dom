31c93ba4bc49c81f9965cf182b21e6c31c310de5
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-09-01 10:46:32 -0500

    components updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
-import Layout from './components/layout/Layout';
-
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -24,6 +24,9 @@ function App() {
 
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import {} from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="centered">
+      <p>Page not found!</p>
+    </div>
+  );
+};
+
+export default NotFound;
