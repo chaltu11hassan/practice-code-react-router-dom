e758597d254ab181b67ae6298e164bc779867a2a
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-09-01 02:09:36 -0500

    file reorganized

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,6 +1,6 @@
 // import { Route } from "react-router-dom";
 
-import QuoteList from "../quotes/QuoteList";
+import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Chaltu", text: "Learning React is fun!" },
